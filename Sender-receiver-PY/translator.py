e52b1328e3756957dda9b1004053cc2715ef5073
guzmanalejandra <77361246+guzmanalejandra@users.noreply.github.com>
2023-08-04 19:00:17 -0600

    .

--- Sender-receiver-PY/translator.py
+++ Sender-receiver-PY/translator.py
@@ -1,5 +1,6 @@
 def string_to_binary(input_string):
-    return ' '.join(format(ord(char), '08b') for char in input_string)
+    binary = ''.join(format(ord(char), '08b') for char in input_string)
+    return binary
 
 def binary_to_string(binary_string):
     binary_values = binary_string.split(' ')
